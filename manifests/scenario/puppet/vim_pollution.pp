1a4540ec2a1bea73e66fe6ce8dbdad7e8cdcec97
Ben Ford <ben.ford@puppetlabs.com>
2016-05-22 23:41:54 -0700

    Add vim pollution scenario

---
+++ manifests/scenario/puppet/vim_pollution.pp
@@ -0,0 +1,13 @@
+class wreckit::scenario::puppet::vim_pollution {
+  $description = "Vim users have this tendency to leave the string `:wq` littered around..."
+  $recovery    = "You should remove `:wq` from puppet.conf."
+  $backup      = undef
+  $urls        = undef
+
+  file_line { 'vim pollution':
+    ensure => present,
+    path   => '/etc/puppetlabs/puppet/puppet.conf',
+    line   => ':wq',
+  }
+
+}
