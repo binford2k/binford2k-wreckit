d6cc51e13e2374b23faa80ce73da4e91bd75c8aa
Ben Ford <ben.ford@puppetlabs.com>
2016-06-13 15:08:30 -0700

    At least catch the error
    I'm not using this for anything now, but it seems interesting and I'd
    like to make it work resiliantly

--- lib/puppet/parser/functions/classes_from_namespace.rb
+++ lib/puppet/parser/functions/classes_from_namespace.rb
@@ -1,7 +1,13 @@
 Puppet::Parser::Functions.newfunction(:classes_from_namespace, :type => :rvalue) do |args|
-  known_resource_types.loader.import_all
-  klasses = known_resource_types.hostclasses.values.collect { |e| e.name }
-  klasses.select! {|e| e.start_with? args.first }
+  begin
+    known_resource_types.loader.import_all
+    klasses = known_resource_types.hostclasses.values.collect { |e| e.name }
+    klasses.select! {|e| e.start_with? args.first }
+  rescue => e
+    Puppet.warning e.message
+    Puppet.debug e.backtrace
+    klasses = []
+  end
 
   klasses
 end
